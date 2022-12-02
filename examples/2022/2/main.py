a4f20336724383cd048c7411d383b9c6baa01cf7
Aron Widforss <aron@antarkt.is>
2022-12-02 09:44:18 +0100

    Use readlines on 2

--- examples/2022/2/main.py
+++ examples/2022/2/main.py
@@ -5,7 +5,7 @@ print("Answer part 1: " + str(
         (res[1] + 1) + ((res[1] - res[0] + 4) % 3 * 3)
         for res in [
             (ord(game[0]) - ord('A'), ord(game[2]) - ord('X'))
-            for game in open(sys.argv[1]).read().split("\n")
+            for game in open(sys.argv[1]).readlines()
         ]
     ])
 ))
@@ -14,7 +14,7 @@ print("Answer part 2: " + str(
         ((res[0] + res[1] - 1) % 3 + 1) + (res[1] * 3)
         for res in [
             (ord(game[0]) - ord('A'), ord(game[2]) - ord('X'))
-            for game in open(sys.argv[1]).read().split("\n")
+            for game in open(sys.argv[1]).readlines()
         ]
     ])
 ))

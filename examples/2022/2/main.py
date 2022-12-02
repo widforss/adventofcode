b808d34f472d34e4af4628e23e5ff771a8428fb7
Aron Widforss <aron@antarkt.is>
2022-12-02 09:01:47 +0100

    Switch to list comprehension in 2

--- examples/2022/2/main.py
+++ examples/2022/2/main.py
@@ -1,23 +1,20 @@
 import sys
-from functools import reduce
 
 print("Answer part 1: " + str(
-    reduce(
-        lambda ack, res: ack + (res[1] + 1) + ((res[1] - res[0] + 4) % 3) * 3,
-        [
+    sum([
+        (res[1] + 1) + ((res[1] - res[0] + 4) % 3 * 3)
+        for res in [
             (ord(game[0]) - ord('A'), ord(game[2]) - (ord('X')))
             for game in open(sys.argv[1]).read().split("\n")
-        ],
-        0
-    )
+        ]
+    ])
 ))
 print("Answer part 2: " + str(
-    reduce(
-        lambda ack, res: ack + ((res[0] + res[1] - 1) % 3 + 1) + (res[1] * 3),
-        [
+    sum([
+        ((res[0] + res[1] - 1) % 3 + 1) + (res[1] * 3)
+        for res in [
             (ord(game[0]) - ord('A'), ord(game[2]) - (ord('X')))
             for game in open(sys.argv[1]).read().split("\n")
-        ],
-        0
-    )
+        ]
+    ])
 ))

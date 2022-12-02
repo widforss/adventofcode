bb406b3b42620b9c6fe2d93008ad8b2af62e6f21
Aron Widforss <aron@antarkt.is>
2022-12-02 09:03:37 +0100

    Remove redundant paretheses

--- examples/2022/2/main.py
+++ examples/2022/2/main.py
@@ -4,7 +4,7 @@ print("Answer part 1: " + str(
     sum([
         (res[1] + 1) + ((res[1] - res[0] + 4) % 3 * 3)
         for res in [
-            (ord(game[0]) - ord('A'), ord(game[2]) - (ord('X')))
+            (ord(game[0]) - ord('A'), ord(game[2]) - ord('X'))
             for game in open(sys.argv[1]).read().split("\n")
         ]
     ])
@@ -13,7 +13,7 @@ print("Answer part 2: " + str(
     sum([
         ((res[0] + res[1] - 1) % 3 + 1) + (res[1] * 3)
         for res in [
-            (ord(game[0]) - ord('A'), ord(game[2]) - (ord('X')))
+            (ord(game[0]) - ord('A'), ord(game[2]) - ord('X'))
             for game in open(sys.argv[1]).read().split("\n")
         ]
     ])

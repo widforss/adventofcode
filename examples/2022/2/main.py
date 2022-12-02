88077612d01e3bff98d7689396b110c7d4a31203
Aron Widforss <aron@antarkt.is>
2022-12-02 08:15:59 +0100

    update 2 with better variable names

--- examples/2022/2/main.py
+++ examples/2022/2/main.py
@@ -3,7 +3,7 @@ from functools import reduce
 
 print("Answer part 1: " + str(
     reduce(
-        lambda a, r: a + (r[0] + 1) + ((r[1] + 4) % 3) * 3,
+        lambda ack, result: ack + (result[0] + 1) + ((result[1] + 4) % 3) * 3,
         [
             (ord(game[2]) - ord('X'), ord(game[2]) - ord(game[0]) - (ord('X') - ord('A')))
             for game in open(sys.argv[1]).read().split("\n")
@@ -13,7 +13,7 @@ print("Answer part 1: " + str(
 ))
 print("Answer part 2: " + str(
     reduce(
-        lambda a, r: a + ((r[0] + r[1] - 1) % 3 + 1) + (r[1] * 3),
+        lambda ack, result: ack + ((result[0] + result[1] - 1) % 3 + 1) + (result[1] * 3),
         [
             (ord(game[0]) - ord('A'), ord(game[2]) - (ord('X')))
             for game in open(sys.argv[1]).read().split("\n")

2474ff25a92780db9afc903639e6f61d027c35d0
Aron Widforss <aron@antarkt.is>
2022-12-02 12:20:11 +0100

    Better variable names

--- examples/2022/2/main.py
+++ examples/2022/2/main.py
@@ -2,8 +2,8 @@ import sys
 
 print("Answer part 1: " + str(
     sum([
-        (res[1] + 1) + ((res[1] - res[0] + 4) % 3 * 3)
-        for res in [
+        (me + 1) + ((me - opponent + 4) % 3 * 3)
+        for opponent, me in [
             (ord(game[0]) - ord('A'), ord(game[2]) - ord('X'))
             for game in open(sys.argv[1]).readlines()
         ]
@@ -11,8 +11,8 @@ print("Answer part 1: " + str(
 ))
 print("Answer part 2: " + str(
     sum([
-        ((res[0] + res[1] - 1) % 3 + 1) + (res[1] * 3)
-        for res in [
+        ((opponent + result - 1) % 3 + 1) + (result * 3)
+        for opponent, result in [
             (ord(game[0]) - ord('A'), ord(game[2]) - ord('X'))
             for game in open(sys.argv[1]).readlines()
         ]

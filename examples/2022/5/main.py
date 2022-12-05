6b4cdc40f8471f218b51be9d4a210972beab0291
Aron Widforss <aron@antarkt.is>
2022-12-05 10:06:51 +0100

    Make solution clearer 5

--- examples/2022/5/main.py
+++ examples/2022/5/main.py
@@ -4,29 +4,29 @@ input = open(sys.argv[1]).read()
 (stacks, steps) = input.split("\n\n")
 stacks = stacks.split("\n")[:-1]
 stacks = [
-    [level[i] for level in reversed(stacks) if level[i] != ' ']
+    [level[i] for level in stacks if level[i] != ' ']
     for i in range(1, len(stacks[0]), 4)
 ]
 steps = [
-    dict(zip(step[::2], map(int, step[1::2])))
+    [int(step[1]), int(step[3]) - 1, int(step[5]) - 1]
     for step in map(str.split, steps.split("\n"))
 ]
 
 def exec_steps(steps, stacks, reverse):
     if not steps:
         return stacks
-    step, steps = steps[0], steps[1:]
-    mv, from1, to1 = step["move"], stacks[step["from"]-1], stacks[step["to"]-1]
-    to2 = to1 + from1[-mv:][::-1 if reverse else 1]
+    mv, from_, to = steps[0]
+    to_stack = stacks[from_][:mv][::-1 if reverse else 1] + stacks[to]
+    from_stack = stacks[from_][mv:]
     stacks = [
-        {step["to"]: to2, step["from"]: from1[:-mv]}.get(i, stack)
-        for i, stack in enumerate(stacks, 1)
+        {to: to_stack, from_: from_stack}.get(i, stack)
+        for i, stack in enumerate(stacks)
     ]
-    return exec_steps(steps, stacks, reverse)
+    return exec_steps(steps[1:], stacks, reverse)
 
 print("Answer part 1: " + ''.join(
-    [stack[-1] for stack in exec_steps(steps, stacks, True)]
+    [stack[0] for stack in exec_steps(steps, stacks, True)]
 ))
 print("Answer part 2: " + ''.join(
-    [stack[-1] for stack in exec_steps(steps, stacks, False)]
+    [stack[0] for stack in exec_steps(steps, stacks, False)]
 ))
